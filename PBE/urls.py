3206d4758b2cfee973ec22d3f6ed1d275b8df4f6
Tu Nombre <you@example.com>
2023-04-20 09:19:40 -0400

    clase-2

--- PBE/urls.py
+++ PBE/urls.py
@@ -2,16 +2,20 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+from django.http import HttpRequest
 from PBE import views
 def otro(request):
     return HttpResponse('otro')
 def otraView(request):
     return HttpResponse('<p>HOLAAA</p>')
+def requestTest(request):
+    return (HttpRequest.body)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testing/',views.testing),
     path('otracosa/',views.algunaOtraCosa),
-    path('otro',otro),
-    path('otraview',otraView)
+    path('otro/',otro),
+    path('otraview',otraView),
+    path('requestTest',requestTest)
 ]
