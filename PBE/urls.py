c875b51a5b496e7a3a67797a10dd98e7000d38d9
Tu Nombre <you@example.com>
2023-04-25 21:26:48 -0400

    clase_recuperativa

--- PBE/urls.py
+++ PBE/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 from django.http import HttpRequest
 from PBE import views
+from posts.api.router import router_post
 def otro(request):
     return HttpResponse('otro')
 def otraView(request):
@@ -17,5 +18,6 @@ urlpatterns = [
     path('otracosa/',views.algunaOtraCosa),
     path('otro/',otro),
     path('otraview',otraView),
-    path('requestTest',requestTest)
+    path('requestTest',requestTest),
+    path('api/',include(router_post.urls))
 ]
